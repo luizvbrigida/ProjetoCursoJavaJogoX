cd0983ff07e20928bba0c6bd9ca5e9045c5ef11a
Luiz Vinicius Brígida <l.vinicius_@hotmail.com>
2021-08-18 17:31:42 -0300

    Implement Board and Pience

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,13 @@
 package application;
 
+import boardgame.Board;
+
+
 public class Program {
 
 	public static void main(String[] args) {
-		System.out.println("Inicio");
+		
+		Board board = new Board(8, 8);
 
 	}
 
---
+++ src/boardgame/Piece.java
@@ -0,0 +1,21 @@
+package boardgame;
+
+public class Piece {
+	
+	protected Position position;
+	private Board board;
+	
+	public Piece() {
+		
+	}
+	
+	public Piece(Position position, Board board) {
+		this.position = null;
+		this.board = board;
+	}
+	
+	protected Board getBoard() {
+		return board;
+	}
+
+}
